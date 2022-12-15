5b118f84a6e9d2f65f1c939dc5141a597e3eb5c6
Dmytro <sovring.t@gmail.com>
2022-12-15 23:47:19 +0200

    Updated coursework1

--- coursework1/script.js
+++ coursework1/script.js
@@ -2,7 +2,25 @@
 window.onload = load;
 const Week = 604800000;
 const Month = 2592000000;
-CalculationResultStorage = [];
+let CalculationResultStorage = [];
+
+let FirstDate;
+let SecondDate;
+
+let DataInputs = document.getElementsByClassName("data-input");
+
+
+ DataInputs[1].addEventListener('input', function(event){
+
+ 	if (DataInputs[0].value == '' && DataInputs[1] !== '') {
+	console.log("no first input")
+ 	} else if (DataInputs[1] !== '' && DataInputs[0] !== '' ) {
+ 		console.log("both inputs");
+ 	}
+})
+
+
+
 
 
 function CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue) {
@@ -44,38 +62,57 @@ function CalculationAction(event) {
 
 	let FirstDate = new Date(document.getElementById('checkin-date').value);
 	let SecondDate = new Date (document.getElementById('checkout-date').value);
+
+
 	let Preset = document.getElementById('preset-selection').value;
-	let Dimension = document.getElementById('Dimension').value;
 	let DaysTypeValue = document.getElementById('DaysType').value;
+	let Dimension = document.getElementById('Dimension').value;
+	
 	let CalculationResult;
+	let SecondDateinMs;
+
+	if (FirstDate > SecondDate || SecondDate < FirstDate) {
+		alert("Кінцева дата не може бути меншою за початкову");
+	}  else {
 
 
-	let FirstDateDay = new Date(FirstDate).toString().slice(0, 3);
-	let SecondDateDay = new Date(SecondDate).toString().slice(0, 3);
+		let FirstDateDay = new Date(FirstDate).toString().slice(0, 3);
+		let SecondDateDay = new Date(SecondDate).toString().slice(0, 3);
 
 
+		if (Preset === "Тиждень") {
+			SecondDateinMs = FirstDate.getTime() + Week;
+			SecondDate = new Date(SecondDateinMs);
+			document.getElementById('checkout-date').value = SecondDate.toISOString().split('.')[0];
+		} else if (Preset === 'Місяць') {
+			SecondDateinMs = FirstDate.getTime() + Month;
+			SecondDate = new Date(SecondDateinMs);
+			document.getElementById('checkout-date').value = SecondDate.toISOString().split('.')[0];
+		} else if (Preset === '') {
+			SecondDate = SecondDate.getTime();
+		}
 
-	if (Preset === "Тиждень") {
-		SecondDate = FirstDate.getTime() + Week;
-		document.getElementById('checkout-date').value = FirstDate.toISOString().split('.')[0];
-	} else if (Preset === 'Місяць') {
-		SecondDate = FirstDate.getTime() + Month;
-		document.getElementById('checkout-date').value = FirstDate.toISOString().split('.')[0];
-	} else if (Preset === '') {
-		SecondDate = SecondDate.getTime();
+	
+		if (DaysTypeValue === 'Всі дні') {
+			CalculationResult = parseInt(durationBetweenDates(FirstDate, SecondDate, Dimension)) + 1;
+		} else { 
+			let CountedDaysinMs = (CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue))*24*60*60000;
+			CalculationResult = CalculateDimension(Dimension, CountedDaysinMs);
+			console.log(CalculationResult);
+		}
+
+		CalculationResultStorage.push(CalculationResult);
+
+
+		document.getElementById('CalculationHistory').innerHTML = CalculationResultStorage;
+
+		localStorage.setItem('LastCalculation', CalculationResultStorage);
+		lastTimeClick();
 	}
+}	
 
-	if (DaysTypeValue === 'Всі дні') {
-		CalculationResult = parseInt(durationBetweenDates(FirstDate, SecondDate, Dimension)) + 1;
-	} else { 
-		let CountedDaysinMs = (CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue))*24*60*60000;
-		CalculationResult = CalculateDimension(Dimension, CountedDaysinMs);
-		console.log(CalculationResult);
-	}
+	
 
-	localStorage.setItem('LastCalculation', parseInt(CalculationResult).toFixed(0));
-	lastTimeClick();
-}
 
 
 function CalculateDimension(Dimension, CalculationResult) {
@@ -115,8 +152,7 @@ function durationBetweenDates(FirstDate, SecondDate, Dimension) {
 
 function lastTimeClick() {
 	 if ("LastCalculation" in localStorage) {
-    	document.getElementById("CalculationHistory").innerHTML = localStorage.LastCalculation;
-    };
+    	document.getElementById("CalculationHistory").innerHTML = localStorage.LastCalculation; }
 };
 
 
