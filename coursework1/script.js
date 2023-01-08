a98f871242a7e0a33c3ec6a4e30b9f1997303727
Dmytro <sovring.t@gmail.com>
2023-01-09 00:13:18 +0200

    updated coursework

--- coursework1/script.js
+++ coursework1/script.js
@@ -4,25 +4,28 @@ const Week = 604800000;
 const Month = 2592000000;
 let CalculationResultStorage = [];
 
-let FirstDate;
-let SecondDate;
+let firstDate = document.getElementById('checkin-date');
+let secondDate = document.getElementById('checkout-date');
 
 let DataInputs = document.getElementsByClassName("data-input");
 
+firstDate.addEventListener('change', () => {
+let block = new Date(firstDate.value).toISOString().split('T')[0];
+secondDate.setAttribute('min', block);
+	document.getElementById("checkout-date").disabled = false;
+});
 
- DataInputs[1].addEventListener('input', function(event){
-
- 	if (DataInputs[0].value == '' && DataInputs[1] !== '') {
-	console.log("no first input")
- 	} else if (DataInputs[1] !== '' && DataInputs[0] !== '' ) {
- 		console.log("both inputs");
- 	}
-})
-
-
+secondDate.addEventListener('change', () => {
+firstDate.setAttribute('max', new Date(secondDate.value).toISOString().split('T')[0]);
+});
 
 
 
+let CalculateButton = document.getElementById('CalculateButton');
+CalculateButton.addEventListener('click', (click) => { this.CalculationAction(click)});
+
+
+//функція для визначення будніх або вихідних днів
 function CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue) {
 
 	let CountedDays = parseInt((durationBetweenDates(FirstDate, SecondDate, 'Порахувати кількість днів' )).split(' ')[0]).toFixed(0);
@@ -57,12 +60,11 @@ function CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue) {
 	return CountedSelectDays;
 }
 
-
+//функція для розрахунку
 function CalculationAction(event) {
 
 	let FirstDate = new Date(document.getElementById('checkin-date').value);
 	let SecondDate = new Date (document.getElementById('checkout-date').value);
-
 
 	let Preset = document.getElementById('preset-selection').value;
 	let DaysTypeValue = document.getElementById('DaysType').value;
@@ -83,63 +85,61 @@ function CalculationAction(event) {
 		if (Preset === "Тиждень") {
 			SecondDateinMs = FirstDate.getTime() + Week;
 			SecondDate = new Date(SecondDateinMs);
-			document.getElementById('checkout-date').value = SecondDate.toISOString().split('.')[0];
+			document.getElementById('checkout-date').value = SecondDate.toISOString().split('T')[0];
 		} else if (Preset === 'Місяць') {
 			SecondDateinMs = FirstDate.getTime() + Month;
 			SecondDate = new Date(SecondDateinMs);
-			document.getElementById('checkout-date').value = SecondDate.toISOString().split('.')[0];
+			document.getElementById('checkout-date').value = SecondDate.toISOString().split('T')[0];
 		} else if (Preset === '') {
 			SecondDate = SecondDate.getTime();
 		}
 
 	
 		if (DaysTypeValue === 'Всі дні') {
-			CalculationResult = parseInt(durationBetweenDates(FirstDate, SecondDate, Dimension)) + 1;
+			CalculationResult = durationBetweenDates(FirstDate, SecondDate, Dimension) ;
 		} else { 
 			let CountedDaysinMs = (CalculationDaysOfWeek(FirstDate, SecondDate, DaysTypeValue))*24*60*60000;
 			CalculationResult = CalculateDimension(Dimension, CountedDaysinMs);
-			console.log(CalculationResult);
 		}
 
-		CalculationResultStorage.push(CalculationResult);
+		
 
-
-		document.getElementById('CalculationHistory').innerHTML = CalculationResultStorage;
-
-		localStorage.setItem('LastCalculation', CalculationResultStorage);
-		lastTimeClick();
+		saveToLocalStorage(FirstDate, SecondDate, CalculationResult);
+		History();
 	}
+	console.log(CalculationResult);
 }	
 
 	
 
 
-
+//функція для розрахунку в потрібній розмірності
 function CalculateDimension(Dimension, CalculationResult) {
 	switch(Dimension) {
         case 'Порахувати кількість днів': 
-        CalculationResult = CalculationResult/60000/60/24;
-        return CalculationResult + ' days';
+        CalculationResult = `${CalculationResult/60000/60/24} days`;
+        return CalculationResult;
         break;
                
         case 'Порахувати кількість годин': 
-        CalculationResult = CalculationResult/60000/60;
-        return CalculationResult + ' hours';
+        CalculationResult = `${CalculationResult/60000/60} hours`;
+        return CalculationResult;
         break;
 
         case 'Порахувати кількість хвилин': 
-        CalculationResult = CalculationResult/60000;
-        return CalculationResult + ' minutes';
+        CalculationResult = `${CalculationResult/60000} minutes`;
+        return CalculationResult;
         break;
 
         case 'Порахувати кількість секунд': 
-        CalculationResult = CalculationResult/1000;
-        return CalculationResult + ' seconds';
+        CalculationResult = `${CalculationResult/1000} seconds`;
+        return CalculationResult;
         break;   
     }
+    console.log(CalculationResult);
 }
 
-
+//фунція для розрахунку різниці дат
 function durationBetweenDates(FirstDate, SecondDate, Dimension) {
 
     let FirstDate1 = new Date(FirstDate);
@@ -150,16 +150,51 @@ function durationBetweenDates(FirstDate, SecondDate, Dimension) {
     return CalculateDimension(Dimension, CalculationResult);
 }
 
-function lastTimeClick() {
-	 if ("LastCalculation" in localStorage) {
-    	document.getElementById("CalculationHistory").innerHTML = localStorage.LastCalculation; }
-};
+//функція для збереження в локалсторадж
+
+function saveToLocalStorage(FirstDate, SecondDate, CalculationResult) {
+
+    const FirstDateFormatted = new Date(FirstDate).toISOString().split('T')[0];
+    const SecondDateFormatted = new Date(SecondDate).toISOString().split('T')[0];
+
+	const CalculationResults = JSON.parse(localStorage.getItem('CalculationResults')) || [];
+	if (CalculationResults.length >= 10) {
+		CalculationResults.shift();
+	}
+	CalculationResults.push({ range: `${FirstDateFormatted} - ${SecondDateFormatted}`, CalculationResult })
+    localStorage.setItem('CalculationResults', JSON.stringify(CalculationResults));
+
+}
+
+//функція для відображення попередніх результатів
+function History() {
+   
+    const table = document.querySelector('.history');
+    const CalculationResults = JSON.parse(localStorage.getItem('CalculationResults'));
+    const tbody = table.querySelector('tbody')
+    table.style.display = 'table';
+    tbody.innerHTML = null;
+
+    CalculationResults.forEach(CalculationResult => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        td2.className = 'resultHistory';
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tbody.appendChild(tr);
+        td1.innerHTML = CalculationResult.range;
+        td2.innerHTML = CalculationResult.CalculationResult;
+    })
+
+}
+
 
 
 function load() {
-    lastTimeClick();
+    History();
 };
 
-let CalculateButton = document.getElementById('CalculateButton');
-CalculateButton.addEventListener('click', (click) => { this.CalculationAction(click)});
+
+
 
