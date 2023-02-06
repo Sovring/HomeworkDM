1ac79142126c0b669407bcadbc64c7008a3ca15a
Dmytro <sovring.t@gmail.com>
2023-02-06 17:24:08 +0200

    updated randomizer

--- HW.IteratorGenerator/Randomizer.js
+++ HW.IteratorGenerator/Randomizer.js
@@ -3,13 +3,13 @@
 
 function* generateRandomNumbers(max, quantity) {
 
-	let count = 0;
+	let count = 1;
 	for (let i = count; i <= quantity; i++) {
 		yield Math.round(Math.random() * max);
 	}
 }
 
-let randomizer = generateRandomNumbers(30, 5);
+let randomizer = generateRandomNumbers(30, 10);
 
 for (let numbers of randomizer) {
 	console.log(numbers);
